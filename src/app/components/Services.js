38df93f035faf95ff59e1f6aafd40dc25f17dc00
DavidTuma <dejvid.tuma@gmail.com>
2024-02-13 10:29:10 +0100

    add data

--- src/app/components/Services.js
+++ src/app/components/Services.js
@@ -9,11 +9,48 @@ import { motion } from "framer-motion";
 //variants
 import { fadeIn } from "/variants";
 
+//Link
+import Link from "next/link";
+
+//data
+const Data = [
+  {
+    image: '/icons/services/PREDAJ.png',
+    name: 'Predaj vozidiel',
+    link: '/',
+  },
+  {
+    image: '/icons/services/DOVOZ.png',
+    name: 'Dovoz vozidiel',
+    link: '/',
+  },
+  {
+    image: '/icons/services/FINANCOVANIE.png',
+    name: 'Financovanie vozidiel',
+    link: '/',
+  },
+  {
+    image: '/icons/services/POISTENIE.png',
+    name: 'Poistenie vozidiel',
+    link: '/',
+  },
+  {
+    image: '/icons/services/VYKUP.png',
+    name: 'Výkup vozidiel',
+    link: '/',
+  },
+  {
+    image: '/icons/services/ZARUKA.png',
+    name: 'Záruka na vozidlo',
+    link: '/',
+  },
+]
+
 export default function Services() {
   return (
-    <section id="services" className="section flex">
+    <section id="services" className="section">
       <div className="container mx-auto">
-        <div className="mb-6 text-left justify-center">
+        <div className="mb-6 justify-center items-center">
           <motion.div className="flex flex-col justify-center">
             <motion.h3
               variants={fadeIn('down', 0.2)}
@@ -22,85 +59,28 @@ export default function Services() {
               viewport={{ once: false, amount: 0.6 }} 
               className="h3 md:h2 text-black">Naše služby<span className="text-accent">.</span></motion.h3>
           </motion.div>
-          <div className="grid grid-cols-2 justify-center">
-            <div>
-              {/* image */}
-              <div>
-                <Image
-                  src={'/icons/services/PREDAJ.png'}
-                  width={80}
-                  height={89}
-                  alt=""
-                />
-              </div>
-              {/* text */}
-              <div>Predaj vozidiel</div>
-            </div>
-            <div>
-              {/* image */}
-              <div>
-                <Image
-                  src={'/icons/services/POISTENIE.png'}
-                  width={80}
-                  height={89}
-                  alt=""
-                />
-              </div>
-              {/* text */}
-              <div>Poistenie vozidiel</div>
-            </div>
-            <div>
-              {/* image */}
-              <div>
-                <Image
-                  src={'/icons/services/DOVOZ.png'}
-                  width={80}
-                  height={89}
-                  alt=""
-                />
-              </div>
-              {/* text */}
-              <div>Dovoz vozidiel</div>
-            </div>
-            <div>
-              {/* image */}
-              <div>
-                <Image
-                  src={'/icons/services/VYKUP.png'}
-                  width={80}
-                  height={89}
-                  alt=""
-                />
-              </div>
-              {/* text */}
-              <div>Výkup vozidiel</div>
-            </div>
-            <div>
-              {/* image */}
-              <div>
-                <Image
-                  src={'/icons/services/FINANCOVANIE.png'}
-                  width={80}
-                  height={89}
-                  alt=""
-                />
-              </div>
-              {/* text */}
-              <div>Financovanie vozidiel</div>
-            </div>
-            <div>
-              {/* image */}
-              <div>
-                <Image
-                  src={'/icons/services/ZARUKA.png'}
-                  width={80}
-                  height={89}
-                  alt=""
-                />
-              </div>
-              {/* text */}
-              <div>Záruka na vozidlo</div>
-            </div>
+          <div className="items-center grid grid-cols-2 xl:grid-cols-3 gap-8">
+            {Data.map((item, index) => {
+              return (
+                <div className="mt-10 text-center hover:border hover:border-accent" key={index}>
+                  {/* image */}
+                  <Link href={item.link}>
+                    <div className="my-4 flex items-center justify-center">
+                      <Image
+                        src={item.image}
+                        width={98}
+                        height={98}
+                        alt=""
+                      />
+                    </div>
+                    {/* text */}
+                    <div className="flex items-center justify-center">
+                      <h3 className="h3 text-accent">{item.name}</h3>
+                    </div>
+                  </Link>
+                </div>
+              )
+            })}
           </div>
         </div>
       </div>
